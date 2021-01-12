a2e10330614059d164f4b1d4f3c42ec145ab59bb
thpietro <thehappypietro@protonmail.com>
2021-01-12 12:03:41 +0100

    Update model-lrs.py
    codice rivisto

--- model-lrs.py
+++ model-lrs.py
@@ -1,12 +1,21 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
+# prima del reshape: [5.1 4.3 3.1 2.6 1.9]
+# dopo il reshape: [[5.1] [4.3] [3.1] [2.6] [1.9]]
 x = np.array([5.1, 4.3, 3.1, 2.6, 1.9]).reshape((-1, 1))
 y = np.array([1.4, 2.2, 3.1, 4.7, 5.4])
 
-model = LinearRegression().fit(x, y)
+# creiamo il modello dall'istanza di LinearRegression
+model = LinearRegression()
+# Alleniamo il modello
+model.fit(x, y)
 
-x = np.array([6]).reshape((-1, 1))
-y_pred = model.predict(x)
+# coeficente di determinazione (dati di ADDESTRAMENTO)
+print('Coeficente di determinazione: %f' % model.score(x, y))
 
-print('minuti previsti:', y_pred)
+# predizione dei dati
+x_pred = np.array([5])
+y_pred = model.predict(x_pred.reshape((-1, 1)))
+
+print('predizione: %f = %f' % (x_pred[0], y_pred[0]))
